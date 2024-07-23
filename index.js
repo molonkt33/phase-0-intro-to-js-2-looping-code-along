e4daba138a2d02f80f21ffcb34ba1c25b73992aa
caleb <kalebntoyian2004@gmail.com>
2024-07-23 15:56:27 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+// index.js
+
+// Function to generate thank you messages
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  // Function to count down and log each number
+  function countDown(start) {
+    while (start >= 0) {
+      console.log(start);
+      start--;
+    }
+  }
+  
